e0ce55ee17ddb1560973cac780af127c3fcb83da
vinicio <vinicio.borja10@gmail.com>
2025-01-27 23:48:41 -0500

    feat: refactor client model to user model and implement user management features

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { SubCategoriesModule } from './sub-categories/sub-categories.module';
 import { SubSubCategoriesModule } from './sub-sub-categories/sub-sub-categories.module';
 import { TicketsModule } from './tickets/tickets.module';
 import { CategoriesOnStaffModule } from './categories-on-staff/categories-on-staff.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CategoriesModule, ClientsModule, StaffModule, CountersModule, SubCategoriesModule, SubSubCategoriesModule, TicketsModule, CategoriesOnStaffModule],
+  imports: [CategoriesModule, ClientsModule, StaffModule, CountersModule, SubCategoriesModule, SubSubCategoriesModule, TicketsModule, CategoriesOnStaffModule, UsersModule],
   controllers: [],
   providers: [],
 })
