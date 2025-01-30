aae674e847156a68a2115424a6b6f46e41e0ac04
vinicio <vinicio.borja10@gmail.com>
2025-01-29 22:38:28 -0500

    feat: implement JWT authentication strategy and add Prisma service for database connection

---
+++ src/users/strategies/jwt.strategy.ts
@@ -0,0 +1,37 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly prisma: PrismaService,
+    configService: ConfigService,
+  ) {
+    super({
+      secretOrKey: configService.get('JWT_SECRET'),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    const { id } = payload;
+
+    const user = await this.prisma.user.findUnique({
+      where: { id: Number(id) },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Token not valid');
+    }
+
+    if (!user.isActive) {
+      throw new UnauthorizedException('User is inactive, talk with an admin');
+    }
+
+    return user;
+  }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -9,23 +9,26 @@ import { JwtModule } from '@nestjs/jwt';
   controllers: [UsersController],
   providers: [UsersService],
   imports: [
+    ConfigModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
 
     //El registerAsync soluciona el problema de que no se puede acceder a process.env.JWT_SECRET
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async () => ({
-        secret: process.env.JWT_SECRET,
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
         signOptions: { expiresIn: '1d' },
       }),
     }),
 
-    //Esto no funciona al momento de lavaantar el servidor, se debe de cambiar por un valor fijo
-    // JwtModule.register({
-    //   secret: process.env.JWT_SECRET,
-    //   signOptions: { expiresIn: '1d' },
-    // }),
+    /*
+      Esto no funciona al momento de lavaantar el servidor, se debe de cambiar por un valor fijo
+      JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+      }),
+    */
   ],
 })
 export class UsersModule {}
