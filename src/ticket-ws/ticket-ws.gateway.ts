5e1bd56acdd89b5140e8c045ac05fa6b836d52a5
vinicio <vinicio.borja10@gmail.com>
2025-02-06 20:23:33 -0500

    feat: implement TicketWebSocket gateway with connection handling, user registration, and client management

--- src/ticket-ws/ticket-ws.gateway.ts
+++ src/ticket-ws/ticket-ws.gateway.ts
@@ -1,7 +1,43 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
 import { TicketWsService } from './ticket-ws.service';
+import { Socket } from 'socket.io';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from 'src/users/interfaces/jwt-payload.interface';
+import { UnauthorizedException } from '@nestjs/common';
 
 @WebSocketGateway({ cors: true })
-export class TicketWsGateway {
-  constructor(private readonly ticketWsService: TicketWsService) {}
+export class TicketWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  constructor(
+    private readonly ticketWsService: TicketWsService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async handleConnection(client: Socket) {
+    console.log('Client connected:', client.id);
+
+    // const token = client.handshake.headers.authentication as string;
+
+    // try {
+    //   const payload = this.jwtService.verify(token) as JwtPayload;
+    //   const user = await this.ticketWsService.getUserFromToken(payload);
+
+    //   await this.ticketWsService.registerClient(client, user);
+    // } catch (error) {
+    //   client.disconnect();
+    //   throw new UnauthorizedException('Token no válido');
+    // }
+  }
+
+  async handleDisconnect(client: Socket) {
+    console.log('Client disconnected:', client.id);
+
+    // await this.ticketWsService.removeClient(client);
+  }
 }
--- src/ticket-ws/ticket-ws.service.ts
+++ src/ticket-ws/ticket-ws.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+
+import { Socket } from 'socket.io';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+interface ConnectedClients {
+  [id: string]: {
+    socket: Socket;
+    user: User;
+  };
+}
 
 @Injectable()
-export class TicketWsService {}
+export class TicketWsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  private connectedClients: ConnectedClients = {};
+
+  async registerClient(client: Socket, user: User) {
+    this.connectedClients[client.id] = { socket: client, user };
+    console.log('Connected clients:', this.connectedClients);
+  }
+
+  async removeClient(client: Socket) {
+    delete this.connectedClients[client.id];
+    console.log('Connected clients:', this.connectedClients);
+  }
+
+  async getUserFromToken(payload: any): Promise<User> {
+    return this.prisma.user.findUnique({
+      where: { id: payload.sub },
+    });
+  }
+}
